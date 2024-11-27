201ee6ad49ddaa8f5aaab29875358d35e856c369
LeonardoR998 <leonardoricci839@gmail.com>
2024-11-27 14:06:10 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>My app</h1>
+      <h2>subtitles</h2>
+    </>
+  );
+}
+
+export default App;
